2ce7e70f15169d92d856973bf00c37386ec8f1e1
T-Roberson <149193083+T-Roberson@users.noreply.github.com>
2024-03-24 20:16:29 -0400

    Task 2 Completed

---
+++ Task_2.py
@@ -0,0 +1,12 @@
+# The Greatest Showdown
+print("Hello, user! Please enter 3 different numbers")
+num1 = int(input(""))
+num2 = int(input(""))
+num3 = int(input(""))
+
+if num1 > num2 and num1 > num3:
+    print((num1))
+elif num2 > num3 and num2 > num1:
+    print((num2))
+else:
+    print(num3)
