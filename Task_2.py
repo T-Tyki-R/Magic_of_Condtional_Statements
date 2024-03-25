29a2d36424c4b7dbd8fcad56d6be5197df96d00c
T-Roberson <149193083+T-Roberson@users.noreply.github.com>
2024-03-24 22:57:30 -0400

    Task 2 Completed

--- Task_2.py
+++ Task_2.py
@@ -5,8 +5,23 @@ num2 = int(input(""))
 num3 = int(input(""))
 
 if num1 > num2 and num1 > num3:
-    print((num1))
+    print((str(num1)) + " is the highest number!")
 elif num2 > num3 and num2 > num1:
-    print((num2))
+    print((str(num2)) + " is the highest number!")
 else:
-    print(num3)
+    print((str(num3)) + " is the highest number!")
+
+if num1 < num2 and num1 < num3:
+    print((str(num1)) + " is the lowest number!")
+elif num2 < num3 and num2 < num1:
+    print((str(num2)) + " is the lowest number!")
+else:
+    print((str(num3)) + " is the lowest number!")
+
+if num1 == num2:
+    print((str(num1)) + " and " + str(num2) + " are the same!")
+elif num1 == num3:
+    print((str(num1)) + " and " + str(num3) + " are the same!")
+else:
+    print((str(num2)) + " and " + str(num3) + " are the same!")
+ 
